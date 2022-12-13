5aa9f6e5a720dde0b86a7677834ddb99a0eb25ed
woejozney <64114008+woejozney@users.noreply.github.com>
2022-12-13 09:56:51 -0600

    initial commit

---
+++ project-1/femi.sql
@@ -0,0 +1,27 @@
+select * from dev.thisisnotarealsqlquery;
+
+select * from dev.humptydumptysatonawall;
+
+select * from dev.thisisnotarealsqlquery;
+
+select * from dev.humptydumptysatonawall;
+
+select * from dev.thisisnotarealsqlquery;
+
+select * from dev.humptydumptysatonawall;
+
+select * from dev.thisisnotarealsqlquery;
+
+select * from dev.humptydumptysatonawall;
+
+select * from dev.thisisnotarealsqlquery;
+
+select * from dev.humptydumptysatonawall;
+
+select * from dev.thisisnotarealsqlquery;
+
+select * from dev.humptydumptysatonawall;
+
+select * from dev.thisisnotarealsqlquery;
+
+select * from dev.humptydumptysatonawall;
